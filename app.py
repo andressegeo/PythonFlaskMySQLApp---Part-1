7d5c38f6997685a18940207e8d5ba14c5b9702ae
andressegeo <andressegeofried@gmail.com>
2019-07-11 00:28:59 +0200

    code reviewed

--- app.py
+++ app.py
@@ -40,7 +40,7 @@ def signUp():
             cursor.callproc('sp_createUser',(_name,_email,_hashed_password))
             data = cursor.fetchall()
 
-            if len(data) is 0:
+            if not data:
                 conn.commit()
                 return json.dumps({'message':'User created successfully !'})
             else:
